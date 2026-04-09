06e1bb94855957cc42e9148b0c1ea17b6b7cbcc4
Singularitty <luispedroferreirinha@gmail.com>
2026-04-09 19:40:30 +0200

    fuck supertab lol

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,6 +2,13 @@ return {
   {
     "noir-lang/noir-nvim",
     ft = { "nr" },
-  },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
+    },
+  }
 }
 
---
+++ lua/plugins/noneckpain.lua
@@ -0,0 +1,11 @@
+return {
+  "shortcuts/no-neck-pain.nvim",
+  version = "*",        -- install latest stable (optional)
+  -- event = "VeryLazy", -- optional: or load on some event
+  config = function()
+    -- Optional: nothing needed, but you *can* configure if you want
+    -- require("no-neck-pain").setup({
+    --   width = 100,  -- e.g. center width
+    -- })
+  end,
+}
